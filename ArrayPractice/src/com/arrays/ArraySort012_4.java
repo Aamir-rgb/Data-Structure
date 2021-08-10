c217ae8cd8dda0566cd9f22d6d8c27a09cc6d9f7
Sidra <Sidra@DESKTOP-RSMA468>
2021-08-10 09:34:41 +0530

    Array Sorting 0 1 and 2 without sorting algorithm.

---
+++ ArrayPractice/src/com/arrays/ArraySort012_4.java
@@ -0,0 +1,52 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArraySort012_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = { 2, 0, 1 };
+		int arr1[] = new int[arr.length];
+		int max = arr[0];
+		int min = arr[0];
+
+		// Copy The Value Into Another Array
+		for (int i = 0; i < arr.length; i++) {
+			arr1[i] = arr[i];
+			// System.out.println(arr1[i]);
+		}
+
+		// Calculate Maximum Element In Array
+		for (int i = 0; i < arr.length; i++) {
+			if (min > arr[1]) {
+				min = arr[i];
+			}
+
+		}
+		arr[0] = min;
+
+		// Calculate Maximum Element In Array
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+
+		}
+		arr[arr.length - 1] = max;
+
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != min && arr1[i] != max) {
+				arr[1] = arr1[i];
+				System.out.println(arr1[i]);
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+}
